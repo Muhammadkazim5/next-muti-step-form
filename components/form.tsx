46a827f71fc3704c1d752d8b4f50e4537bac487a
Muhammad Kazim <133555661+Muhammadkazim5@users.noreply.github.com>
2026-05-17 11:08:50 +0500

    Clean up phone field error handling code
    Removed commented-out error message display for phone field.

--- components/form.tsx
+++ components/form.tsx
@@ -306,11 +306,11 @@ export default function Form() {
                         autoComplete='phone'
                         className='block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
                       />
-                      {/* {errors.phone?.message && (
+                      { {errors.phone?.message && (
                       <p className='mt-2 text-sm text-red-400'>
                         {errors.phone.message}
                       </p>
-                    )} */}
+                    )} }
                     </div>
                   </div>
                   <div className='mb-3'>
